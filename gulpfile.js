5df118c15de452afc2d76e38cf2f2de2628ee9a6
Roman <Ranerg@yandex.ru>
2014-07-06 18:27:11 +0900

    Prevent GulpJS from crashing on error. Work with new packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp'),
+    gulputil = require('gulp-util'),
+    plumber = require('gulp-plumber'),
     changed = require('gulp-changed'),
     autoprefixer = require('gulp-autoprefixer'),
     csscomb = require('gulp-csscomb'),
@@ -10,8 +12,10 @@ var gulp = require('gulp'),
     haml = require('gulp-haml'),
     htmlprettify = require('gulp-html-prettify'),
     livereload = require('gulp-livereload'),
+    rename = require('gulp-rename'),
+    replace = require('gulp-replace'),
     lr = require('tiny-lr');
-	server = lr();
+server = lr();
 
 paths = {
     less: 'less/*.less',
@@ -19,39 +23,65 @@ paths = {
     haml: 'haml/*.haml',
 };
 
+var onError = function(error) {
+    gulputil.beep();
+    gulputil.log(error);
+};
+
 gulp.task('less', function() {
     return gulp.src('less/*.less')
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(changed('./public/', {
             extension: '.css'
         }))
         .pipe(less())
-        .pipe(autoprefixer(["last 40 version", "ie 10", "ie 9", "ie 8", "ie 7"], { cascade: true }))
+        .pipe(autoprefixer(["last 40 version", "ie 11", "ie 10", "ie 9", "ie 8", "ie 7"], {
+            cascade: true
+        }))
         .pipe(csscomb())
         .pipe(gulp.dest('./public/'))
         .pipe(minifycss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('./public/min'))
         .pipe(livereload(server));
 });
 
 gulp.task('coffee', function() {
     return gulp.src('coffee/*.coffee')
-        .pipe(changed('./public/', {extension: '.js'}))
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(changed('./public/', {
+            extension: '.js'
+        }))
         .pipe(coffee())
         .pipe(gulp.dest('./public/'))
         .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest('./public/min'))
         .pipe(livereload(server));
 });
 
 gulp.task('haml', function() {
     return gulp.src('haml/*.haml')
+        .pipe(plumber({
+            errorHandler: onError
+        }))
         .pipe(changed('./public/', {
             extension: '.html'
         }))
         .pipe(haml())
-        .pipe(gulp.dest('./public/min'))
         .pipe(htmlprettify())
         .pipe(gulp.dest('./public/'))
+        .pipe(replace(/href="((.(?!.min))*).css"/gm, 'href="$1.min.css"'))
+        .pipe(replace(/src="((.(?!.min))*).js"/gm, 'src="$1.min.js"'))
+        .pipe(gulp.dest('./public/min'))
         .pipe(livereload(server));
 });
 
